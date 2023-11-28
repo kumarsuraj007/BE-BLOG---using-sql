4105c5ba2b3eba8e75b947b44c8523977163b6f1
Suraj Kumar <109417154+kumarsuraj007@users.noreply.github.com>
2023-11-28 19:57:23 +0530

    create register/login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='auth'>
+      <h1>Login</h1>
+      <form action="">
+        <input type="text" placeholder='username' />
+        <input type="text" placeholder='password' />
+        <button>Login</button>
+        <span>Don't you have an account? <Link to='/register'>Register</Link></span>
+      </form>
+    </div>
   )
 }
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='auth'>
+      <h1>Register</h1>
+      <form action="">
+        <input required type="text" placeholder='username' />
+        <input required type="email" placeholder='email' />
+        <input required type="password" placeholder='password' />
+        <button>Login</button>
+        <span>Do you have an account? <Link to='/Login'>Login</Link></span>
+      </form>
+    </div>
   )
 }
 
